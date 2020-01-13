0cec62e4aefcf94ef9cf7cb5a5fd0b8ebb5bbcc4
Berat DINCKAN <cedinckan@gmail.com>
2020-01-13 17:37:53 +0300

    yorumlar eklendi

--- tr/örnek/websocket_1/client.py
+++ tr/örnek/websocket_1/client.py
@@ -5,12 +5,12 @@ DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 4 #
 
 humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-sio = socketio.Client()
+sio = socketio.Client() # client açma işlemi
 connected = False
 time.sleep(5)
 while not connected:
     try:
-        sio.connect("http://10.11.22.179:5000")
+        sio.connect("http://10.11.22.179:5000") #socketio ya bağlanılması
         connected = True
     except:
         print("baglaniliyor")
@@ -19,9 +19,10 @@ sayac = 0
 succes = False
 
 while True:
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)# sensör değerinin alınması
     msg="sayac = {0} nem = {1} , sicaklik = {2}".format(sayac,humidity,temperature)
-    sio.emit("message",msg)
+    sio.emit("message",msg)#emit send gibi bir komut ve emit sayesinde istenilen event'e veri gönderebiliyoruz
+    #msg nin olduğu yere callback fonsiyonu da gelebilir aslında biz sadece değişken değil fonksiyon da gönderebiliyoruz
     sayac = sayac + 1
     time.sleep(1)
     if sayac == 101:
--- tr/örnek/websocket_1/main.py
+++ tr/örnek/websocket_1/main.py
@@ -4,17 +4,20 @@ import eventlet
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
-eventlet.monkey_patch(socket=True)
+eventlet.monkey_patch(socket=True) # eventletin aktif edilmesi
 #socketio = SocketIO(app)
-socketio = SocketIO(app,async_mode="eventlet",log_output=True)
+socketio = SocketIO(app,async_mode="eventlet",log_output=True) #terminalde olayların gösterilmesi için log_output=True yapıldı
+#aysnc_mode = "eventlet" projenin eventlet üzerinde koşması için
 app.debug = True
 @app.route("/")
 def index():
 	return render_template("index.html")
-@socketio.on('message')
-def handleMessage(msg):
+@socketio.on('message') # message eventi olduğu zaman çalışacak fonksiyon
+def handleMessage(msg): # handleMessage fonskiyonunun ismi önemli değil önemli olan yukarıdaki event
+#fonsiyonda tutulan msg socketio nun getirdiği en büyük özelliklerden birisi
+# bu sayede hiçbir ek kod yazmadan msg değişkenini client'dan alabiliyor
 	print('Message: ' + msg)
-	send(msg, broadcast=True)
+	send(msg, broadcast=True) # alınan verinin yayınlanabilmesi için broadcast =True yaptık
 
 if __name__ == '__main__':
 	socketio.run(app,host ="10.11.22.179")
